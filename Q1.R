9329c50f41d7de8ae3a0b1187f2085d47c248510
Julien Pierre <jupierr@microsoft.com>
2015-02-14 20:40:44 +0800

    Finished quizz week 2

--- Q1.R
+++ Q1.R
@@ -1,5 +1,7 @@
 question1 <- function() {
   library(httr)
+  library(httpuv)
+  library(jsonlite)
   
   # 1. Find OAuth settings for github:
   #    http://developer.github.com/v3/oauth/
@@ -11,7 +13,7 @@ question1 <- function() {
   #
   #    Insert your client ID and secret below - if secret is omitted, it will
   #    look it up in the GITHUB_CONSUMER_SECRET environmental variable.
-  myapp <- oauth_app("github", key="3c17cd24cdc0b197c05d")
+  myapp <- oauth_app("github", key="3c17cd24cdc0b197c05d", secret="f29e17b086ebce2aa2fa4df883485fb04d9e8b1b")
   
   # 3. Get OAuth credentials
   github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
@@ -20,5 +22,7 @@ question1 <- function() {
   gtoken <- config(token = github_token)
   req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
   stop_for_status(req)
-  content(req)
+  res <- content(req)
+  json <- jsonlite::fromJSON(toJSON(res))
+  json[c("created_at","url")]
 }
